92ad3af4badfcf938e105eb53fa30a35ee466dec
vicmaburrito <vmaldaraca@gmail.com>
2022-03-01 14:42:15 -0600

    Fix navbar style and add animation for h1

--- src/components/FirstBlock/FirstBlock.js
+++ src/components/FirstBlock/FirstBlock.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './FirstBlock.css';
+import background from '../../assets/images/FirstBlockbackground.png';
 
 function FirstBlock() {
   return (
-    <section className="masthead d-flex align-items-center">
+    <section className="masthead d-flex align-items-center" style={{ background: `url(${background})` }}>
       <div className="container px-4 px-lg-5 text-center">
-        <h1 className="mb-1 display-1 text-white">Manuel Aldaraca</h1>
+        <h1 className="mb-1 display-1 tracking-in-expand text-white">Manuel Aldaraca</h1>
         <h3 className="mb-5 display-3 text-white"><em>Hello There!</em></h3>
       </div>
     </section>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,48 +1,78 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import ItemNav from './itemNav';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faBars } from '@fortawesome/free-solid-svg-icons';
+// import ItemNav from './itemNav';
 import './navbar.css';
 
 // DONE: Create Item navbar component as button and export it
 function Navbar() {
-  const scrollTo = (id) => {
-    const element = document.getElementById(id);
-    element.scrollIntoView({
-      behavior: 'smooth',
-    });
-  };
+  // const scrollTo = (id) => {
+  //   const element = document.getElementById(id);
+  //   element.scrollIntoView({
+  //     behavior: 'smooth',
+  //   });
+  // };
 
   return (
-    <nav className="navbar navbar-expand-lg">
-      <ItemNav
-        onClick={() => scrollTo('firstBlock')}
-        item="Manuel Aldaraca"
-      />
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <FontAwesomeIcon icon={faBars} />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <ItemNav
-              onClick={() => scrollTo('about')}
-              item="About Me"
-            />
-          </li>
-          <li className="nav-item">
-            <ItemNav
-              onClick={() => scrollTo('about')}
-              item="Projects"
-            />
-          </li>
-          <li className="nav-item">
-            <ItemNav
-              onClick={() => scrollTo('contact')}
-              item="Contact"
-            />
-          </li>
-        </ul>
+    // <header className="header">
+    //   <nav className="navbar navbar-expand-lg">
+    //     <ItemNav
+    //       onClick={() => scrollTo('firstBlock')}
+    //       item="Manuel Aldaraca"
+    //     />
+    //     <button className="navbar-toggler" type="button"
+    //     data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+    //   aria-expanded="false" aria-label="Toggle navigation">
+    //       <FontAwesomeIcon icon={faBars} />
+    //     </button>
+    //     <div className="collapse navbar-collapse" id="navbarNav">
+    //       <ul className="navbar-nav">
+    //         <li className="nav-item">
+    //           <ItemNav
+    //             onClick={() => scrollTo('about')}
+    //             item="About Me"
+    //           />
+    //         </li>
+    //         <li className="nav-item">
+    //           <ItemNav
+    //             onClick={() => scrollTo('about')}
+    //             item="Projects"
+    //           />
+    //         </li>
+    //         <li className="nav-item">
+    //           <ItemNav
+    //             onClick={() => scrollTo('contact')}
+    //             item="Contact"
+    //           />
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </nav>
+    // </header>
+    <nav className="navbar fixed-top navbar-expand-lg navbar-dark ">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#some">Manuel</a>
+        {/* <ItemNav
+          onClick={() => scrollTo('firstBlock')}
+          item="Manuel Aldaraca"
+        /> */}
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#id">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#id">Course</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#id" tabIndex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
