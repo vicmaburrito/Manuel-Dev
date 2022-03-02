496052b653be00f29214a016b08bb8173cab89eb
vicmaburrito <vmaldaraca@gmail.com>
2022-03-02 16:27:26 -0600

    Fix background issue

--- src/components/FirstBlock/FirstBlock.js
+++ src/components/FirstBlock/FirstBlock.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
-import TOPOLOGY from 'vanta/dist/vanta.topology.min';
-import * as p5 from 'p5';
+// import TOPOLOGY from 'vanta/dist/vanta.topology.min';
+// import * as p5 from 'p5';
+import FOG from 'vanta/dist/vanta.fog.min';
+import * as THREE from 'three';
 import './FirstBlock.css';
 
 function FirstBlock() {
@@ -9,9 +11,21 @@ function FirstBlock() {
   useEffect(() => {
     if (!vantaEffect) {
       setVantaEffect(
-        TOPOLOGY({
+        FOG({
           el: myRef.current,
-          p5,
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.00,
+          minWidth: 200.00,
+          highlightColor: 0x2222,
+          midtoneColor: 0x89964e,
+          lowlightColor: 0x3d5021,
+          baseColor: 0x064635,
+          blurFactor: 0.28,
+          speed: 2.20,
+          zoom: 1.80,
         }),
       );
     }
@@ -22,7 +36,7 @@ function FirstBlock() {
   return (
     <section className="masthead d-flex align-items-center" ref={myRef}>
       <div className="container px-4 px-lg-5 text-center scale-down-center">
-        <h1 className="mb-1 display-3 text-white">Manuel Aldaraca</h1>
+        <h1 className="mb-1 display-4 text-white">Manuel Aldaraca</h1>
         <h3 className="mb-5 text-white"><em>Full-Stack Web Developer</em></h3>
       </div>
     </section>
